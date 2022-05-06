6c93f2dbb7c4a387dae15264bbaf316bd475f7a2
barnedo <78799951+barnedo@users.noreply.github.com>
2022-05-06 13:20:26 -0300

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,13 @@
+function sumar(){
+	
+	var n1 = parseInt(document.getElementById('txt1').value);
+	var n2 = parseInt(document.getElementById('txt2').value);
+
+	var r =  n1 + n2;
+
+
+	
+	
+
+	document.getElementById('res').innerHTML = r;
+}
